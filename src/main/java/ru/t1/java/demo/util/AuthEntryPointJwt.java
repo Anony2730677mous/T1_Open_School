d939309615b0a3476d7e2ab55533a95a1966d8c8
Anony2730677mous <104105@mail.ru>
2024-10-31 11:18:13 +0300

    fix small problem

--- src/main/java/ru/t1/java/demo/util/AuthEntryPointJwt.java
+++ src/main/java/ru/t1/java/demo/util/AuthEntryPointJwt.java
@@ -1,7 +1,6 @@
 package ru.t1.java.demo.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +22,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException)
 
-            throws IOException, ServletException {
+            throws IOException {
         log.error("Unauthorized error: {}", authException.getMessage());
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
